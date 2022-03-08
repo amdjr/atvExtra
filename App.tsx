70acce930110a675c85be65ba040e7e80f7a1511
@mdj <84931002+amdjr@users.noreply.github.com>
2022-03-08 03:16:47 -0300

    comit ok

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { useFonts } from 'expo-font';
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import clearSansRegular from './src/fonts/ClearSans-Regular.ttf';
+import clearSansBold from './src/fonts/ClearSans-Bold.ttf';
+import Routes from './src/routes/Routes';
+
+import { AuthProvider } from './src/contexts/Auth';
+
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {  
+
+  const [fontsLoaded] = useFonts({
+    clearSansRegular,
+    clearSansBold,
+  });
+ 
+  React.useEffect(() => {}, [fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <NavigationContainer>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+        <StatusBar hidden />
+    </NavigationContainer>
+  );
+}
+
